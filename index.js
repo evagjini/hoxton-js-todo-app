fab9a3ac2b7bf314d63d5277d7eda2c147461308
Fevronia Gjini <fevronigjini1@gmail.com>
2022-06-24 14:17:48 +0200

    chngs

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 // first step 
 
+let state = {
+    todos:[
+        
+        {text:'Go Shopping', completed:false },
 
-let stat=[
-    { title: "Go Shopping", completed:false}
-    {title: "Work out", completed:false}
+        {text:'Work out', completed: false },
 
-]
-function renderOptionsSection(){
+        {text:'See Doctor', completed:true},
+        {text:'Eat Healthy', completed: false},
+
+    ],
+
+    showcompleted:true,
+    
+}
+
+
+   function renderApp() {
+    let appEl = document.createElement('div')
+    appEl.className = 'app'
+    document.body.append(appEl)
+  }
+
+function renderOptionsSections(){
     let optionsSection = document.createElement('section');
     optionsSection.className= ('options');
     let optionsTitle = document.createElement('h2');
@@ -23,9 +40,54 @@ function renderOptionsSection(){
     showCompletedLabel.append('Show completed' , showCompletedInput)
     optionsSection.append(optionsTitle,showCompletedLabel )
 
+
+
+    let appEl = document.querySelector('.app')
+    appEl?.append(optionsSection)
+
 }
 
 
-function render()
+function render(){
+    document.body.textContent=''
 
-// function renderOptionsSection()
+renderApp()
+renderOptionsSections()
+
+}
+
+render()
+
+
+
+
+// function getIncompleteTodos(){
+//     return state.todos.filter(todo=>todo.completed===false)
+// }
+// function getCompletedTodos(){
+//     return state.todos.filter(todo=>todo.completed===true)
+// }
+
+// function getTodoToDisplay(){
+//     if (state.showcompleted) return state.todos
+//     else return getCompletedTodos()
+// }
+// <div class="app">
+//         <section class="options">
+//         <h2 class="sec-title">Options</h2>
+//             <label>
+//                 Show Completed
+//                 <input type="checkbox"/>
+//             </label>
+//         </section>
+
+
+
+
+
+
+
+
+
+
+// 

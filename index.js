069ea30532853a5a5b2d1d223130991800c754c6
Fevronia Gjini <fevronigjini1@gmail.com>
2022-06-27 02:09:27 +0200

    nnot final

--- index.js
+++ index.js
@@ -15,84 +15,232 @@ let state = {
     showcompleted:true,
     
 }
-
-
-   function renderApp() {
-    let appEl = document.createElement('div')
-    appEl.className = 'app'
-    document.body.append(appEl)
+// input: none
+// action: gets the incomplete todos
+// output: the incomplete todos
+    function getIncompleteTodos() {
+    return state.todos.filter(todo => todo.completed === false)
+  }
+  function getCompleteTodos() {
+    return state.todos.filter(todo => todo.completed === true)
   }
 
-function renderOptionsSections(){
-    let optionsSection = document.createElement('section');
-    optionsSection.className= ('options');
-    let optionsTitle = document.createElement('h2');
-    optionsTitle.className=('sec-title');
-    optionsTitle.innerText='Options';
-    
-
-
-    let showCompletedLabel=document.createElement('label')
-    let showCompletedInput=document.createElement('input')
-    showCompletedInput.type='checkbox'
-
-
-    showCompletedLabel.append('Show completed' , showCompletedInput)
-    optionsSection.append(optionsTitle,showCompletedLabel )
-
-
-
-    let appEl = document.querySelector('.app')
-    appEl?.append(optionsSection)
+  function getTodosToDisplay() {
+  if (state.showCompleted) return state.todos
+  else return getIncompleteTodos()
 
 }
 
 
 
-    function renderAddTodoSection (){
-    let addTodoSection = document.createElement('section')
-    addTodoSection.className= ('add-item')
-    let addTitle= document.createElement('h2')
-    addTitle.className=('sec-title')
-    addTitle.innerText= 'Add Item'
 
-     addTodoSection.append(addTitle)
+function createTodo(text) {
+    // check if the todo is in the list
+    let foundMatch = state.todos.some(todo => todo.text === text)
+    if (foundMatch) return // guard statement
+  
+    state.todos.push({ text: text, completed: false })
+  }
+
+
+  
+function deleteTodo(text) {
+    let updatedTodos = state.todos.filter(todo => todo.text !== text)
+    state.todos = updatedTodos
+  }
+  function deleteAllTodos() {
+    state.todos = []
+  }
+
+function toggleTodo(text) {
+    // find the todo we want to toggle
+    let match = state.todos.find(todo => todo.text === text)
+    if (!match) return
+  
+    // if it exists, toggle it
+    match.completed = !match.completed
+  }
+ 
+function toggleShowCompleted() {
+    state.showCompleted = !state.showCompleted
+  }
+   function renderApp() {
+    let appEl = document.createElement('div')
+    appEl.className = 'app'
+    document.body.append(appEl)
+  }
+  
+  
+
+
+function renderOptionsSections(){
+
+    let optionsSection = document.createElement('section');
+
+    let optionsTitle = document.createElement('h2');
+    optionsTitle.className=('title');
+    optionsTitle.innerText='OPTIONS';
+  
+    
+
+    let showCompletedLabel=document.createElement('label')
+    showCompletedLabel.textContent= 'Show Completed :'
+
+
+    let showCompletedInput=document.createElement('input')
+    showCompletedInput.type='checkbox'
+
+     optionsSection.append(optionsTitle)
+    showCompletedLabel.append(showCompletedInput)
+    optionsSection.append(optionsTitle, showCompletedLabel)
+  
+    let appEl = document.querySelector('.app')
+
+    appEl.append(optionsSection)
+    render()
+   
+}
+
+
+    function renderAddItems (){
+    let addItemsSection = document.createElement('section');
+    // addTodoSection.className= ('add-item')
+
+    let title= document.createElement("h2")
+    title.className= "title"
+    title.textContent= "Add Item"
+
+    let form = document.createElement("form")
+    form.className = "add-item"
+
+    let textInput = document.createElement('input');
+    textInput.type= 'text'
+    textInput.placeholder= 'Add your todo here ......'
+
+    let addTodoButton = document.createElement('button')
+    addTodoButton.textContent= 'Add button'
+
+
+
+
+    form.addEventListener ('Submit', function (event){
+       event.preventDefault()
+       createTodo(textInput.value)
+       render()
+
+    })
+       
+     
+
+     form.append(textInput, addTodoButton)
+     addItemsSection.append(title,form)
+
+
+
+    let appEl = document.querySelector('.app')
+    appEl.append(addItemsSection)
+
+    //  document.body.append(form)
 
     }
 
 
+    function renderTodoSection(){
+        let todoSection = document.createElement('section')
 
+        let title= document.createElement('h2')
+        title.className= 'title'
+        title.textContent= 'TODO'
+
+
+
+        let todoList= document.createElement('ul')
+        todoList.className = 'todo-list'
+        
+        let incompleTodos = state.todos.filter(todo => todo.completed === false)
+        
+        for (let todo of incompleTodos){
+            let todoItemEl = document.createElement('li')
+            todoItemEl.className = 'todo'
+
+           
+          let checkboxEl = document.createElement('input') 
+          checkboxEl.type = 'checkbox' 
+          checkboxEl.className= 'checkbox'
+
+
+          let todoTextEl = document.createElement('p')
+          todoTextEl.className= 'todo-text'
+          todoTextEl.innerText= todo.text
+
+
+          let deleteButton= document.createElement('button')
+          deleteButton.className= 'todo-delete'
+          deleteButton.textContent= 'DELETE'
+
+
+         todoItemEl.append(checkboxEl, todoTextEl,deleteButton)
+         todoList.append(todoItemEl)
+
+        }
+        
+
+        todoSection.append(title, todoList)
+
+        
+        appEl?.append(todoSection)
+        render()
+
+    }
+
+function renderCompletedTodos(){
+    let section = document.createElement('section')
+   
+
+    let title= document.createElement('h2')
+    title.className= 'title'
+    title.textContent= 'COMPLETED'
+
+
+    let ul = document.createElement('ul')
+    ul.className='list-completed'
+    section.append(title, ul)
+
+    
+
+
+
+    let appEl= document.querySelector('.app')
+    appEl.append(section);
+
+}
 
 function render(){
     document.body.textContent=''
 
 renderApp()
 renderOptionsSections()
-renderAddTodoSection()
+renderAddItems()
+renderTodoSection()
+renderCompletedTodos()
 
 }
 
 render()
 
 
-// <section class="add-item">
-//         <h2 class="sec-title"> Add item </h2>
-//            
+
+
+
+// if (state.showcompleted) showCompletedInput.checked =true
+// showCompletedInput.addEventListener('click',function (){
+//     toggleShowCompleted()
+//     render()
+// })
 
 
 
 
-
-
-
-
-
-
-//                    <form>
-// //                 <input type="text" />
-// //                 <button>Add</button>
-// //             </form>
-//         </section>
 
 
 
